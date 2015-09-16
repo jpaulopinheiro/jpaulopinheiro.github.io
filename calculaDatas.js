5add414f361b86910d62d96b58f1adafb8331026
jpinheiro <jpinheiro@jpinheiro-laptop>
2015-09-15 23:09:39 -0300

    Melhoria

--- calculaDatas.js
+++ calculaDatas.js
@@ -1,8 +1,13 @@
 $(function() {
 	moment.locale('pt-BR');
     $("#data-venda").val(obterDataAtualFormatada());
+    $("#botao-calcular").click(calcularDatas())
 });
  
 function obterDataAtualFormatada() {
 	return moment().format('L');
+}
+
+function calcularDatas() {
+	$("#resultados").hide();
 }
