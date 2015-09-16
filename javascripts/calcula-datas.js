3207d29179e80a2ed505f0e66012d6bc7e418a77
05487496633 <05487496633@serpro-1539682>
2015-09-16 10:59:34 -0300

    Teste

--- javascripts/calcula-datas.js
+++ javascripts/calcula-datas.js
@@ -1,6 +1,6 @@
 $(function() {
 	moment.locale('pt-BR');
-    $("#data-venda").val(obterDataAtualFormatada());
+    $("#data-venda").val(moment().toDate());
     $("#botao-calcular").click(calcularDatas);
     $("#botao-limpar").click(limpar);
     $("#botao-limpar").prop( "disabled", true );
