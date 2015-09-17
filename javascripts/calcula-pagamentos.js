677ba9a832251b7b984e4734dac1c4830c4df8b0
jpinheiro <jpinheiro@jpinheiro-laptop>
2015-09-17 15:21:33 -0300

    .

--- javascripts/calcula-pagamentos.js
+++ javascripts/calcula-pagamentos.js
@@ -109,7 +109,7 @@ function calcularValores(valorVenda, tipoCartao, bandeiraCartao, numeroParcelas)
 	var valorParcela = valorBruto.div(numeroParcelas);
 
 	if(ehDizimaPeriodica(valorParcela.toString())){
-		valorParcela.round(2,0);
+		valorParcela = valorParcela.round(2,0);
 		var ajuste = valorBruto.minus(valorParcela.times(numeroParcelas));
 		var valorParcela1 = Big(valorParcela.plus(ajuste));
 		
