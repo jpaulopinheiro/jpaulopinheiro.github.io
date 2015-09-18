5bcba213aab4b56966c9e03a69bfd95d0abb9b64
jpinheiro <jpinheiro@jpinheiro-laptop>
2015-09-17 21:46:44 -0300

    .

--- javascripts/calcula-pagamentos.js
+++ javascripts/calcula-pagamentos.js
@@ -134,7 +134,7 @@ function calcularValores(valorVenda, tipoCartao, bandeiraCartao, numeroParcelas)
 
 function calcularValorLiquidoParcela(valorBrutoParcela, tipoCartao, bandeiraCartao, numeroParcelas){
 	var fator = Big(new Big(1).minus(obterPercentualDesconto(tipoCartao, bandeiraCartao, numeroParcelas)));
-	var valorLiquidoParcela = valorBrutoParcela.times(fator.valueOf());
+	var valorLiquidoParcela = valorBrutoParcela.times(paserFloat(fator.toString()));
 	valorLiquidoParcela = valorLiquidoParcela.round(2,1);
 	return valorLiquidoParcela;
 }
