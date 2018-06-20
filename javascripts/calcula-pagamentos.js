58cc4489397c76e4762b6a412ad6206d2df9bdcc
jpaulopinheiro <jpaulo.pinheiro@hotmail.com>
2018-06-20 13:09:35 -0300

    Atualização de índices de acordo com o combinado com a CIELO.

--- javascripts/calcula-pagamentos.js
+++ javascripts/calcula-pagamentos.js
@@ -257,25 +257,25 @@ function obterPercentualDesconto(tipoCartao, bandeiraCartao, numeroParcelas){
 		} else {
 			switch(numeroParcelas){
 				case 1:
-					percentualDesconto = new Big(0.0315);
+					percentualDesconto = new Big(0.0252);
 					break;
 				case 2:
 				case 3:
-					percentualDesconto = new Big(0.039);
+					percentualDesconto = new Big(0.0327);
 					break;
 				case 4:
 				case 5:
 				case 6:
-					percentualDesconto = new Big(0.0415);
+					percentualDesconto = new Big(0.0352);
 					break;
 				default:
-					percentualDesconto = new Big(0.0465);
+					percentualDesconto = new Big(0.0402);
 					break;
 			}
 		}
 	} else if(tipoCartao == 2){
 		// cartão de débito	
-		percentualDesconto = new Big(0.0218);
+		percentualDesconto = new Big(0.0174);
 	}
 	return percentualDesconto;
 }
